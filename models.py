d8925893b16377f12012dba39d983ecec563a653
am3lmeireles-tech <am3lmeireles@gmail.com>
2026-06-09 19:22:30 -0300

    Tela diario, sugestao e editar

--- models.py
+++ models.py
@@ -3,11 +3,26 @@ from datetime import date
 
 db = SQLAlchemy()
 
-class Diario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    texto = db.Column(db.Text, nullable=False)
-
 class RegistroHumor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     humor = db.Column(db.String(50), nullable=False)
     data = db.Column(db.Date, nullable=False, default=date.today)
+
+# tabela diário (Amanda)
+class Diario(db.Model):
+
+    __tablename__ = "diario"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True
+    )
+
+    texto = db.Column(
+        db.Text,
+        nullable=False
+    )
+
+    data = db.Column(
+        db.String(50)
+    )
