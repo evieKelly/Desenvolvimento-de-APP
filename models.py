2afd317694b847ec58511f7ffcb85a21c23605fb
Luiz Filipy <luizfilipyrf@gmail.com>
2026-06-09 20:32:10 -0300

    Remove classe Diario duplicada que impedia o app de iniciar

--- models.py
+++ models.py
@@ -13,10 +13,6 @@ class Usuario(db.Model):
     senha = db.Column(db.String(255), nullable=False)
 
 
-class Diario(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    texto = db.Column(db.Text, nullable=False)
-
 class RegistroHumor(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     humor = db.Column(db.String(50), nullable=False)
